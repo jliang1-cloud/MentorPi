1a0a7c1bce22f9d2def039d0bdf7fd201c3c393c
jliang1-cloud <jliang1@swarthmore.edu>
2026-07-27 09:12:52 -0400

    Remove unused 'need_compile' variable
    Removed unused environment variable 'need_compile' from launch setup.

--- driver/controller/launch/controller.launch.py
+++ driver/controller/launch/controller.launch.py
@@ -10,7 +10,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction, TimerAction, OpaqueFunction
 
 def launch_setup(context):
-    compiled = os.environ['need_compile']
     namespace = LaunchConfiguration('namespace', default='')
     use_namespace = LaunchConfiguration('use_namespace', default='false').perform(context)
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
