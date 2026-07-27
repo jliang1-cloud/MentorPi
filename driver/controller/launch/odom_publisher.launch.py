59cfb5d12a78fda5028d939a7bd15cee7b3dc5d6
jliang1-cloud <jliang1@swarthmore.edu>
2026-07-27 09:13:13 -0400

    Remove unused 'need_compile' variable from launch file
    Removed unused environment variable 'need_compile'.

--- driver/controller/launch/odom_publisher.launch.py
+++ driver/controller/launch/odom_publisher.launch.py
@@ -8,7 +8,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
 
 def generate_launch_description():
-    compiled = os.environ['need_compile']
     namespace = LaunchConfiguration('namespace', default='')
     use_namespace = LaunchConfiguration('use_namespace', default='false')
     odom_frame = LaunchConfiguration('odom_frame', default='odom')
