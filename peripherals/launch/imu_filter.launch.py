47de8a8c1d4b779246a13f74e90e9f7b2a2d4126
jliang1-cloud <jliang1@swarthmore.edu>
2026-07-28 09:33:58 -0400

    Fix remapping paths in imu_filter.launch.py

--- peripherals/launch/imu_filter.launch.py
+++ peripherals/launch/imu_filter.launch.py
@@ -18,8 +18,8 @@ def generate_launch_description():
         output='screen',
         parameters=[{"calib_file": calib_file_path}],
         remappings=[
-            ('raw', 'ros_robot_controller/imu_raw'),
-            ('corrected', 'imu_corrected')
+            ('/raw', 'ros_robot_controller/imu_raw'),
+            ('/corrected', 'imu_corrected')
         ]
     )
 
